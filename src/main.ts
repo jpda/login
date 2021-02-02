42f4649f184f9ef8579a7f6d09d1a8940c9f45ed
jpda <john.dandison@live.com>
2021-02-02 13:12:15 -0500

    subscription id issue

--- src/main.ts
+++ src/main.ts
@@ -88,9 +88,9 @@ class LoginInfo {
     public constructor(allowNoSubscriptionsLogin?: boolean, enableAzPsSession?: boolean, subscriptionId?: string, environment?: string, resourceManagerEndpointUrl?: string, tenantId?: string) {
         this.AllowNoSubscriptionsLogin = allowNoSubscriptionsLogin ? allowNoSubscriptionsLogin : core.getInput('allow-no-subscriptions').toLowerCase() === "true";
         this.EnableAzPsSession = enableAzPsSession ? enableAzPsSession : core.getInput('enable-AzPSSession').toLowerCase() === "true";
-        this.SubscriptionId = subscriptionId ? subscriptionId : core.getInput('enable-AzPSSession').toLowerCase();
+        this.SubscriptionId = subscriptionId ? subscriptionId : core.getInput('subscriptionId').toLowerCase();
         this.Environment = environment ? environment : core.getInput('environment').toLowerCase();
-        this.ResourceManagerEndpointUrl = resourceManagerEndpointUrl ? resourceManagerEndpointUrl : core.getInput('environment').toLowerCase();
+        this.ResourceManagerEndpointUrl = resourceManagerEndpointUrl ? resourceManagerEndpointUrl : core.getInput('resourceManagerEndpointUrl').toLowerCase();
         this.TenantId = tenantId ? tenantId : core.getInput('tenantId').toLowerCase();
     }
 }
